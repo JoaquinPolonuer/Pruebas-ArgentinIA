29bd9faf85491a3917be48fa051691a2b3240746
joaquinpolonuer <jtpolonuer@gmail.com>
2023-03-17 12:30:56 -0300

    Label de algunas imagenes y data para el clssfier

---
+++ create_classifier_dataset.py
@@ -0,0 +1,27 @@
+import os
+import json
+import cv2 
+import uuid
+
+if not os.path.exists("classifier_data/"):
+    os.mkdir("classifier_data/")
+
+images_paths = os.listdir('data/images')
+labels_paths = [path.replace('png', 'json') for path in images_paths]
+
+for image_path, label_path in zip(images_paths, labels_paths):
+    image = cv2.imread(os.path.join('data/images', image_path))
+    with open(os.path.join('data/labels', label_path)) as f:
+        labels = json.load(f)
+
+    for i, shape in enumerate(labels['shapes']):
+        label = shape['label']
+        (x_min, y_min), (x_max, y_max) = shape['points']
+        x_min, y_min, x_max, y_max = int(x_min), int(y_min), int(x_max), int(y_max)
+        if not os.path.exists(os.path.join('classifier_data/', label)):
+            os.mkdir(os.path.join('classifier_data/', label))
+        
+        name = f"{i}{image_path}"
+
+        cv2.imwrite(os.path.join('classifier_data/', label, name), image[y_min:y_max, x_min:x_max])
+    
